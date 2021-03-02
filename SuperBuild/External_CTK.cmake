d45aae89a6f26d9140569cccd892be63468cebf8
Csaba Pinter <pinter.csaba@gmail.com>
2021-03-02 09:15:19 +0000

    ENH: Update CTK to include DICOM database improved filtering

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -64,13 +64,15 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_REPOSITORY
-    "${EP_GIT_PROTOCOL}://github.com/commontk/CTK.git"
+    # "${EP_GIT_PROTOCOL}://github.com/commontk/CTK.git"
+    "${EP_GIT_PROTOCOL}://github.com/verdure-imaging-inc/CTK.git"
     QUIET
     )
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "9a9573ec4e0653ee96fe02823ac7ee66b40d3b44"
+    # "9a9573ec4e0653ee96fe02823ac7ee66b40d3b44"
+    "afa1ea90147c6df4590f2ea0ff48fa259d4882cf" # verdure-imaging-inc/dicom-table-additional-where-conditions
     QUIET
     )
 
